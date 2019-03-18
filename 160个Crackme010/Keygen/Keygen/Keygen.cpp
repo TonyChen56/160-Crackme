744c7ff487c774d74db131f25ac6e6e38eaba17d
陈子轩 <873217126@qq.com>
2019-03-18 20:02:55 +0800

    Add files via upload

---
+++ 160个Crackme010/Keygen/Keygen/Keygen.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+
+int CalcKey()
+{
+	char result[MAX_PATH] = { 0 };
+	char key[MAX_PATH] = { "kXy^rO|*yXo*m\\kMuOn*+" };
+	int keyLen = strlen(key);
+
+	for (int i = 0; i < keyLen; i++)
+	{
+		result[i] = key[i] - 0xA;
+	}
+
+	printf("%s\n", result);
+
+	return 0;
+}
+
+
+
+
+int main()
+{
+	CalcKey();
+	system("pause");
+	return 0;
+}
