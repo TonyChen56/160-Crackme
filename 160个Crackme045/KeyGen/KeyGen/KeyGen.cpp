f856579e6d304854973585cd65016da08d1ea92a
陈子轩 <873217126@qq.com>
2019-05-28 08:35:27 +0800

    Add files via upload

---
+++ 160个Crackme045/KeyGen/KeyGen/KeyGen.cpp
@@ -0,0 +1,25 @@
+﻿#include "pch.h"
+#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <stdio.h>
+
+
+int main()
+{
+	int result = 0x37;
+	char username[20] = { 0 };
+	char serial[20] = { 0 };
+	printf("请输入用户名：");
+	scanf_s("%s", username, 20);
+	int usernameLen = strlen(username);
+	if (usernameLen>=4)
+	{
+		for (int i = 0; i < usernameLen; i++)
+		{
+			result += username[i] << 9;
+		}
+	}
+	printf("%d\n", result);
+	system("pause");
+	return 0;
+}
